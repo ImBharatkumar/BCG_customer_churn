ca803c1bec6596a0d4ea3b3b8c09754e4f630624
Bharatkumar Kori <bharatkumargkori@gmail.com>
2024-01-28 15:57:31 +0530

    updation

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,65 @@
+import numpy as np 
+import os, sys
 import pandas as pd
-import os
+import logging
+import pickle
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+import joblib
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import make_scorer
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 
 
-#function to load data
-def load_data(path):
-    data= pd.read_csv(path)
-    return data
 
-# function to save df to csv file
-import os
+def evaluate_models(X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series, models, param):
+    model_report = {}
+    best_model_accuracy = 0
+    best_model = None
 
-def save_to_csv(df, path):
-    if not os.path.exists(os.path.dirname(path)):
-        try:
-            os.makedirs(os.path.dirname(path))
-        except:
-            print("Error: unable to create directory for csv")
-    df.to_csv(path, index=False)
+    for model_name, model in models.items():
+        logging.info(f"Evaluating {model_name}")
+        for params in param[model_name]:
+            model.set_params(**params)
+            scores = cross_val_score(model, X_train, y_train, cv=5)
+            accuracy = scores.mean()
+            model_report[model_name] = accuracy
+
+            if accuracy > best_model_accuracy:
+                best_model_accuracy = accuracy
+                best_model = model_name
+
+    model_report['best_model'] = best_model
+    model_report['best_accuracy'] = best_model_accuracy
+
+    return model_report
+
+
+def save_object(file_path, obj):
+    with open(file_path, 'wb') as f:
+        joblib.dump(obj, f)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+    
